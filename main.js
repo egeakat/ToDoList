e6f2eb30ac6df9d27910c5872510964d01f031c7
b21807052 <b21807052@cs.hacettepe.edu.tr>
2019-02-07 00:54:20 +0300

    Adding a task part is almost complete. I lost too much time picking the css and yet I can do the javascript parts in like 10 mins :/

--- main.js
+++ main.js
@@ -8,13 +8,53 @@ window.onload = function(){
     }, 50);
     
 let addTask = document.getElementById("add-task");
-
-taskNode = document.createElement("tr");
-console.log(taskNode);
-/*
+let add   = document.getElementById("add");
+let i = 0;
+let table = document.getElementById("table");
+let taskIsActive = false
 addTask.onclick = function(){
+    if(taskIsActive){
+        alert("Please fill out the task before adding a new one");
+        return false;
+    }
+    let taskNode = document.createElement("tr");
+    let addButton = document.createElement("span");
+
+
+    let txtTask = document.createElement("textarea");
+    let txtDue =  document.createElement("textarea");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    taskNode.id = String(i++);
+    txtTask.id = String(i) + "-1";
+    txtDue.id = String(i) + "-2";
+
+    txtTask.cols  = "20";
+    txtDue.cols = "20";
+
+    txtTask.rows = "1";
+    txtDue.rows = "1";
+
+    addButton.id = "add";
+    addButton.innerHTML = "Add";
+    td1.appendChild(txtTask);
+    td2.appendChild(txtDue);
+    td3.appendChild(addButton);
+    taskNode.appendChild(td1);
+    taskNode.appendChild(td2);
+    taskNode.appendChild(td3);
+    table.appendChild(taskNode);
+    taskIsActive = true;
     
-}*/
+};
+
+add.onclick = function(){
+  //  console.log(txtSays.value);
+};
+
+
+
 };
 
 
